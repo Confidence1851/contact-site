f581091285b757c500b6e617305cba7e0f3e70e2
Eazygee <82817023+Eazygee@users.noreply.github.com>
2021-06-25 15:35:36 +0100

    changes in the Models

--- app/Models/contact.php
+++ app/Models/contact.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'contactname',
+        'phonenumber',
+        'completed'
+    ];
+    }
 
-    protected $guarded = [];
-}
